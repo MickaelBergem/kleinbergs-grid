6af87fac473fd664554abc3287cbfa5cf7568aec
Mickaël Bergem <mickael@securem.eu>
2016-02-24 17:54:08 +0100

    Bugged code trying to graph mean steps for different values of r

--- kleinberg.py
+++ kleinberg.py
@@ -5,12 +5,14 @@ Kleinberg's grid
 @author: Mickaël Bergem
 """
 from chrono import chrono
+from multiprocessing import Pool
+import matplotlib.pyplot as plt
 import numpy as np
 
 # Grid size
-N = 10000
+N = 100
 # Kleinberg parameter
-r = 2
+r = 2.
 
 # Shortcuts grid, defaults to -1
 shortcuts = np.empty((N, N))
@@ -34,7 +36,7 @@ def new_shortcut(x, y):
     The probability of returning node B is proportional to d(A, B)^-r
     """
     # Generate all the distance value for the grid
-    local_distance_grid = global_distance_grid[N-x-1:2*N-x-1, N-y-1:2*N-y-1]
+    local_distance_grid = global_distance_grid[N-x-1:2*N-x-1, N-y-1:2*N-y-1].copy()
 
     # Normalize all these values to get the distribution
     local_distance_grid /= local_distance_grid.sum()
@@ -59,14 +61,14 @@ def distance(ax, ay, bx, by):
 
 
 @chrono
-def build_distance_grid():
+def build_distance_grid(r):
     """
     Fill the distance grid
 
     We assume that the reference node is at (N, N). Thanks to symmetry we can
     build the full distance grid, centered on the reference node.
     """
-
+    print("r:", r)
     for i in range(N):
         for j in range(1 if i==0 else 0, N):
             d = distance(0, 0, i, j) ** (-r)
@@ -74,7 +76,6 @@ def build_distance_grid():
             global_distance_grid[N-i-1, N+j-1] = d
             global_distance_grid[N-i-1, N-j-1] = d
             global_distance_grid[N+i-1, N-j-1] = d
-
 
 # Distance grid for future use
 gridfilename = 'global_distance_grid{}.npy'.format(N)
@@ -86,12 +87,102 @@ try:
     print("Loaded the global distance grid from file.")
 except IOError:
     global_distance_grid = np.zeros((2*N-1, 2*N-1))
-    build_distance_grid()
+    build_distance_grid(r)
+    print("Saving the grid in {}".format(gridfilename))
     np.save(gridfilename, global_distance_grid)
 
 
+def random_dir(buffer=1000):
+    while True:
+        rands = np.random.randint(2, size(buffer))
+        for i in range(buffer):
+            yield rands[i]
+
+def routing(_):
+    """ Applies the routing algorithm and returns the number of steps """
+    start_x, start_y, dest_x, dest_y = np.random.randint(N, size=(4))
+    # print("Starting node: {}:{}".format(start_x, start_y))
+    # print("Destination node: {}:{}".format(dest_x, dest_y))
+
+    steps = 0
+    current_x = start_x
+    current_y = start_y
+
+    # Compute maxsteps (if no shortcut)
+    maxsteps = distance(start_x, start_y, dest_x, dest_y)
+
+    while current_x != dest_x or current_y != dest_y:
+        # New step
+        steps += 1
+        current_distance = distance(current_x, current_y, dest_x, dest_y)
+        # print("Step #{}: {}:{}, distance={}".format(steps, current_x, current_y, current_distance))
+
+        # Grab shortcut
+        shortcut = get_shortcut(current_x, current_y)
+        sx = shortcut // N
+        sy = shortcut % N
+        s_distance = distance(sx, sy, dest_x, dest_y)
+
+        if s_distance < current_distance - 1:
+            # It is better to use the shortcut than the neighbors
+            current_x = sx
+            current_y = sy
+            continue
+
+        # It is better (or equal) to use the neighbors
+        if current_x == dest_x:
+            # Move along y
+            direction = 1
+        elif current_y == dest_y:
+            # Move along x
+            direction = 0
+        else:
+            # uniform
+            direction = random_dir()
+
+        if direction == 1:
+            current_y += 1 if dest_y > current_y else -1
+        else:
+            current_x += 1 if dest_x > current_x else -1
+
+    # print("Finished in {} steps!".format(steps))
+    return (steps, maxsteps)
+
+@chrono
+def init_random():
+    random_dir(100000)
+
+@chrono
+def run_routing(r, Ntries):
+    """ Run Ntries routing for the given value of r """
+    build_distance_grid(r)
+    print("Running {} routings...".format(Ntries))
+
+    steps = pool.map(routing, range(Ntries))
+    meanstep, meanmaxstep = np.sum(steps, axis=0)/Ntries
+    print("Routed using {:.2f} steps on average".format(meanstep))
+    return meanstep, meanmaxstep
+
 
 if __name__ == '__main__':
-    print(shortcuts.shape)
-    # print(global_distance_grid)
-    print(get_shortcut(1,1))
+
+    Ntries = 10000
+    pool = Pool(8)
+
+    r_list = list(np.arange(.1, 3, .1))
+    meansteps = []
+    meanmaxsteps = []
+
+    init_random()
+
+    for r in r_list:
+        meanstep, meanmaxstep = run_routing(r, Ntries)
+        meansteps.append(meanstep)
+        meanmaxsteps.append(meanmaxstep)
+
+    np.save('graph_meansteps_r{}_N{}_Ntries{}.npy'.format(r, N, Ntries), meansteps)
+
+    plt.plot(r_list, meansteps, label="Mean steps")
+    # plt.plot(r_list, meanmaxsteps, label="Max steps (if no shortcuts)")
+    plt.legend()
+    plt.show()
--- notes.md
+++ notes.md
@@ -24,3 +24,37 @@ Building the initial distance grid takes 3'40" on a single core, for N = 10000
 (time complexity = O(N²)), and the grid is cached on disk.
 
 This state is referenced by the git tag `basic-shortcut-finder`.
+
+## The routing algorithm
+
+Here I used no special technique but instead implemented the routing algorithm.
+We only compute the shortcut if we need it, and if it has a equivalent distance
+to what provide the neighbors, we don't use it.
+If both neighbors allow to progress, we uniformly pick one.
+
+I used parallelization to use all my cores.
+
+I then call `run_routing` for different values of `r`.
+
+This gives the following (surprising) graph for N=100 and Ntries=10000:
+
+[!graph1](graph1.png)
+
+Crap, I did not write my code to use different values of r in the first place,
+and now I am always using the same r!
+
+## MemoryError
+
+Quickly enough I notice that the memory consumption is quite huge and that I
+need to reduce the problem. I chose to come back to my initial idea, that I
+rejected, because I thought... hmm well... Actually the idea was good (just a
+bit more complex to implement):
+
+1. we compute the number of destination nodes at distance `d`, as if the grid was infinite;
+* the distribution is then "vector of number of nodes at distance i" * "d ^ (-r) for all the possible distances";
+* pick a distance d according to the distribution;
+* uniformly pick a node at this distance: if the node is out of the grid, start over from step 3.
+
+By doing so, we remove the solutions out of the grid from the potential
+destination nodes, which is equivalent to removing them from the initial
+distribution since we try again.
